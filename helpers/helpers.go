0830177fe9692e57d60c20e7637f366b2ba1cd2d
can <21026424+nzmcno@users.noreply.github.com>
2023-09-19 14:11:31 +0100

    ScanFileReturnAnts  returns

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -30,13 +30,19 @@ func ContainsSpace(s string) bool {
 	return false
 }
 
-func ScanFileReturnAnts(file *os.File)  {
+func ScanFileReturnAnts(file *os.File)(int, [][]string, [][]string) {
 	// Create a scanner to read lines from the file.
 	scanner := bufio.NewScanner(file)
+	var links [][]string
+	var rooms [][]string
+	var temprooms [][]string
+	var startRoom []string
+	var endRoom []string
 
 	TotalAnts := 0
 	mindex := 0         // Initialize line index
-	isEnd := false      // Initialize isEnd check
+	IsEnd := false      // Initialize isEnd check
+	IsStart := false    // init start check
 	isFirstLine := true // Initialize isFirstLine check
 	// Loop through each line in the file.
 	for scanner.Scan() {
@@ -44,18 +50,20 @@ func ScanFileReturnAnts(file *os.File)  {
 		if isFirstLine {
 			ants, _ := strconv.Atoi(line)
 			TotalAnts = ants
-			fmt.Println("Total ants:" , TotalAnts)
+			fmt.Println("Total ants:", TotalAnts)
 			isFirstLine = false
 		} else if strings.HasPrefix(line, "##start") {
 			fmt.Println("Start:", line)
+			IsStart = true
 			// Check for lines starting with "##start" and "##end" and print them.
 		} else if strings.HasPrefix(line, "##end") {
 			fmt.Println("End:", line)
-			isEnd = true
+			IsEnd = true
 		} else if strings.Contains(line, "-") {
 			// For other lines, split them by '-' and print the parts.
 			parts := strings.Split(line, "-")
 			if len(parts) == 2 {
+				links = append(links, parts)
 				fmt.Printf("From: %s, To: %s\n", parts[0], parts[1])
 			} else {
 				fmt.Println("Invalid line format:", line)
@@ -63,15 +71,22 @@ func ScanFileReturnAnts(file *os.File)  {
 		} else {
 			parts := strings.Split(line, " ")
 			if len(parts) == 3 {
-			 	if isEnd {
-					isEnd = false
-					// add to the end 
+				if IsStart {
+					IsStart = false
+					// add to the Start
+					startRoom = parts
+					fmt.Printf("START FOUND Name: %s, X: %s, Y: %s\n", parts[0], parts[1], parts[2])
+				} else if IsEnd {
+					IsEnd = false
+					// add to the end
+					endRoom = parts
 					fmt.Printf("END FOUND Name: %s, X: %s, Y: %s\n", parts[0], parts[1], parts[2])
 				} else {
 					// add to array
+					temprooms = append(temprooms, parts)
 					fmt.Printf("Name: %s, X: %s, Y: %s\n", parts[0], parts[1], parts[2])
 				}
-				
+
 			} else {
 				fmt.Println("Invalid line format:", line)
 			}
@@ -84,4 +99,14 @@ func ScanFileReturnAnts(file *os.File)  {
 		fmt.Println("Error reading the file:", err)
 	}
 
+	// Concatenate slices
+	rooms = append(rooms, startRoom)    // Adding startRoom to rooms
+	rooms = append(rooms, temprooms...) // Adding temprooms to rooms
+	rooms = append(rooms, endRoom)      // Adding endRoom to rooms
+
+	// Print concatenated slice
+	for _, room := range links {
+		fmt.Println(room)
+	}
+	return TotalAnts, rooms, links
 }
--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 
 func main() {
 	if len(os.Args) >= 2 {
-		
-	// Open the input file for reading. Replace "input.txt" with your actual file name.
-	file, err := os.Open("examples/"+os.Args[1])
-	if err != nil {
-		fmt.Println("Error opening the file:", err)
-		return
-	}
-	defer file.Close()
 
-	helpers.ScanFileReturnAnts(file)
+		// Open the input file for reading. Replace "input.txt" with your actual file name.
+		file, err := os.Open("examples/" + os.Args[1])
+		if err != nil {
+			fmt.Println("Error opening the file:", err)
+			return
+		}
+		defer file.Close()
+
+		helpers.ScanFileReturnAnts(file)
 	}
-	
+
 }
